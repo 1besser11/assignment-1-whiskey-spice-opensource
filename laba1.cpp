e826e6b133fce0c0b5cd6eec4f780cf1eb105583
Slavick-Vinnitskyi <andrey.belyi1@gmail.com>
2017-03-27 09:20:46 +0300

    dodelovanie

--- laba1.cpp
+++ laba1.cpp
@@ -1,44 +1,47 @@
-#include <iostream>
+#include <stdio.h>
 #include <fstream>
 #include <iostream>
+#include <string.h>
 
 using namespace std;
+const int len = 60 , strings = 21;
+
+char mass[len][strings];
+typedef struct 
+{
+    int Points;
+    char* Team;
+}Table;
+Table APL[20];
+char* score;
 
 int main()
 {
-const int len = 60 , strings = 20;
-
-const char eol = '\n';
-char mass[len][strings];
-
 ifstream fs("/home/slavick/Рабочий стол/Labs/premier_league.csv",ios:: in|ios:: binary);
 if(!fs)
  {
  fprintf(stderr, "File premier_league.csv was not opened \n");
  return 1;
- 
  }
-for(int i=0; i<strings;i++)
-{
 
-fs.getline(mass[i], len -1, eol);
-cout<< mass[i]<<endl;
-}
-
-for(int j=0;j<strings;j++)
-{
-    for(int k=0;k < len;k++)
+else{
+ 
+ for(int i=0; i<strings;i++)
+ {
+   if(mass[i]!="\0")
     {
-     if (mass[k]!= "\n")
-     
-     
-     else break;
+   fs.getline(mass[i], len , '\n');
+   cout<< mass[i]<<endl;
     }
-    cout<<endl;
+ }
+ for (int i=1;i<strings;i++)
+ {   
+        APL[i].Team = strtok(mass[i],",");
+        cout<<APL[i].Team<<endl;   
+ }
+ 
 
-}
-
-fs.close();
-return 0;
-
+ fs.close();
+ return 0;
+  }
 }
