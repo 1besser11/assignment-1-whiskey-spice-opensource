dbaa77762bb36d24408ecc78b89c6a56fa1a8f88
Slavick-Vinnitskyi <andrey.belyi1@gmail.com>
2017-03-28 09:14:10 +0300

    pochti final

---
+++ source.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include<fstream>
+//Граб Халеп
+using namespace std;
+struct Team
+{
+    string name;
+    string result;
+    int point;
+};
+int  input_count();
+void sort(Team * ,int);
+Team* input();
+void out(Team * ptr, int a);
+int main(int argc, char *argv[])
+{
+	int count = input_count();
+    Team * ptr = input();
+    sort(ptr, count);
+	out(ptr, count);
+    return 0;
+}
+
+int input_count()
+{
+	ifstream fin("premier_league.csv");
+    int a;
+    fin >> a;
+	return a;
+}
+
+Team* input()
+{
+    ifstream fin("premier_league.csv");
+    int a;
+    fin >> a;
+    Team *ptr = new Team[a];
+    for(int i = 0; i < a; ++i)
+    {
+        getline(fin,ptr[i].name, ',');
+        getline(fin,ptr[i].result, '\n');
+    }
+    for(int i = 0; i < a; ++i)
+    {
+        cout << ptr[i].name ;
+        cout << ptr[i].result << endl;
+    }
+    return ptr;
+}
+
+void sort(Team * ptr,int a)
+{
+    int match = 10;
+    for(int i = 0; i < a; ++i)
+    {
+        for(int j = 0; j < 40 ; j += 4)
+        {
+            if((int)ptr[i].result[j] > (int)ptr[i].result[j+2])
+                ptr[i].point += 3;
+            if((int)ptr[i].result[j] == (int)ptr[i].result[j+2])
+                ++ptr[i].point;
+            cout << ptr[i].point << "  ";
+
+
+        }
+        cout << ptr[i].name << "  " <<ptr[i].point << endl;
+    }
+    for(int i = 0 ; i < a -1 ; i++ )
+    {
+        for( int j = 0; j < a-i-1; j++)
+        {
+            if( ptr[j].point < ptr[j+1].point)
+            {
+                Team temp = ptr[j+1];
+                ptr[j+1] = ptr[j];
+                ptr[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < a; i++)
+     {
+	 	 cout << ptr[i].name << "  " <<ptr[i].point << endl;
+ 	}
+ }
+ 
+ void out(Team * ptr, int a)
+ {
+	ofstream fout;
+    fout.open("results.csv", ios_base::out);
+    for (int g = 0; g < a; g++)
+     {
+	 	fout << ptr[g].name << "  " <<ptr[g].point << endl;
+ 	 }
+	fout.close();
+}
